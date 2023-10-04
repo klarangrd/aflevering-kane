d9792702736625da5a56f7737406fafd26564676
klarangrd <klara.ngaard@gmail.com>
2023-10-04 13:25:57 +0200

    restart js

--- script.js
+++
@@ -1,7 +0,0 @@
-fetchContent("/albums.json").then((data) => {
-    async function fetchContent(url) {
-    let request = await fetch(url);
-    let json = await request.json();
-     return json;
-    },
-};
